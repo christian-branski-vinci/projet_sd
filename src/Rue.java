10d3887b09328cc8abfe89828ed451a981fdbcea
christian-branski-vinci <christian.branski@student.vinci.be>
2026-03-09 11:29:13 +0100

    add Rue

---
+++ src/Rue.java
@@ -0,0 +1,27 @@
+public class Rue {
+
+  double distance;
+  String nom;
+
+  public Rue(double distance, String nom) {
+    this.distance = distance;
+    this.nom = nom;
+  }
+
+  public double getDistance() {
+    return distance;
+  }
+
+  public void setDistance(double distance) {
+    this.distance = distance;
+  }
+
+  public String getNom() {
+    return nom;
+  }
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+}
