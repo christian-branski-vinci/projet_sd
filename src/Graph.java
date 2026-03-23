415842b754cbb140bc6310ae08d3a26e32c0c9dc
vinci-aida-amirloei <aida.amirloei@student.vinci.be>
2026-03-23 10:52:48 +0100

    fixing the test

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 public class Graph {
@@ -11,10 +12,12 @@ public class Graph {
     public Graph(String localisations, String roads) {
         mapRueAdjacentes = new HashMap<>();
         mapLocalisations = new HashMap<>();
-
+        lectureFichier(localisations,roads);
+    }
+    private void lectureFichier(String localisations, String roads) {
         // Lire les noeuds
         try (Scanner sc = new Scanner(new File(localisations))) {
-            sc.nextLine(); // sauter l'en-tête
+            sc.nextLine();
             while (sc.hasNextLine()) {
                 String[] parts = sc.nextLine().split(",");
                 Long id = Long.parseLong(parts[0]);
@@ -24,12 +27,15 @@ public class Graph {
                 double alt = Double.parseDouble(parts[4]);
                 Localisation loc = new Localisation(id, lat, lon, nom, alt);
                 mapLocalisations.put(id, loc);
+                mapRueAdjacentes.put(id, new ArrayList<>());
             }
-        } catch (Exception e) { e.printStackTrace(); }
+        } catch (IOException e) {
+            throw new RuntimeException("Erreur lors de la lecture du fichier de localisations : " + localisations, e);
+        }
 
         // Lire les arcs
         try (Scanner sc = new Scanner(new File(roads))) {
-            sc.nextLine(); // sauter l'en-tête
+            sc.nextLine();
             while (sc.hasNextLine()) {
                 String[] parts = sc.nextLine().split(",");
                 Long idOrigine = Long.parseLong(parts[0]);
@@ -38,12 +44,13 @@ public class Graph {
                 String nom = parts[3];
                 Localisation origine = mapLocalisations.get(idOrigine);
                 Localisation arrivee = mapLocalisations.get(idArrivee);
-                Rue rue = new Rue(dist,nom,origine,arrivee);
-                mapRueAdjacentes.computeIfAbsent(idOrigine, k -> new ArrayList<>()).add(rue);
+                Rue rue = new Rue(dist, nom, origine, arrivee);
+                mapRueAdjacentes.get(idOrigine).add(rue);
             }
-        } catch (Exception e) { e.printStackTrace(); }
+        } catch (IOException e) {
+            throw new RuntimeException("Erreur lors de la lecture du fichier des routes : " + roads, e);
+        }
     }
-
     public Localisation[] determinerZoneInondee(long[] idsOrigin,double epsilon) {
         //TODO
         List<Localisation> visitees = new ArrayList<>();
@@ -128,7 +135,41 @@ public class Graph {
 
     public Map<Localisation,Double> determinerChronologieDeLaCrue(long[] idsOrigin, double vWaterInit, double k) {
         //TODO
-        return null ;
+        Map<Localisation, Double> chronologie = new HashMap<>();
+        for (int i = 0; i < idsOrigin.length; i++) {
+
+            determinerChronologieDeLaCrueBis(idsOrigin[i], vWaterInit, k, chronologie);
+        }
+
+        return chronologie;
+    }
+
+    public void determinerChronologieDeLaCrueBis(long idOrigin, double vWaterInit, double k, Map<Localisation, Double> map){
+        Localisation localisationOrigin = mapLocalisations.get(idOrigin);
+        if (map.containsKey(localisationOrigin)){
+            return;
+        }
+        List<Rue> list = mapRueAdjacentes.get(idOrigin);
+        double temps = Double.MAX_VALUE;
+        Localisation localisationTempsDest= new Localisation(null,0,0,null,0);
+        double vitesse= 0;
+        for (Rue rue : list) {
+            Localisation localisationDest = mapLocalisations.get(rue.arrivee.getId());
+            double pente = (localisationOrigin.getAltitude() - localisationDest.getAltitude())/rue.getDistance();
+            double vitesseEau= vWaterInit + (k * pente);
+            double tempsRue = rue.getDistance()/vitesseEau;
+            if (tempsRue<temps){
+                temps= tempsRue;
+                localisationTempsDest= localisationDest;
+                vitesse= vitesseEau;
+            }
+        }
+        if (localisationTempsDest.getId()==null){
+            return;
+        }
+        map.put(localisationOrigin, temps);
+        determinerChronologieDeLaCrueBis(localisationTempsDest.getId(), vitesse,k , map);
+
     }
 
     public Deque<Localisation> trouverCheminDEvacuationLePlusCourt(long idOrigin, long idEvacuation, double vVehicule, Map<Localisation,Double> tFlood) {
